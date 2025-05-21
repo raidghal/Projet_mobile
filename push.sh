d2ddb610470945dfaae4f50e81f54d9d864a6127
raidghal <sm.raid.ghallabi@gmail.com>
2025-05-21 23:51:24 +0200

    Mise à jour du projet

--- push.sh
+++ push.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cd "$(dirname "$0")"
+set -e
+PROJET_DIR=$(pwd)
+cd "$PROJET_DIR"
+git init
+git remote remove origin 2> /dev/null || true
+git remote add origin https://github.com/raidghal/Projet_mobile.git
 git add .
-echo "Message de commit : "
-read msg
-git commit -m "$msg"
-
+git commit -m "Mise à jour du projet"
+git branch -M main
+git push -u origin main --force
